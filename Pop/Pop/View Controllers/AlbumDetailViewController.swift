1d4663e7896d44b7de0f066181282c60d914d6a1
Markus Jahnsrud <markus@soriko.no>
2019-10-28 19:53:59 +0100

    Added .gitignore + more project config

---
+++ Pop/Pop/View Controllers/AlbumDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AlbumDetailViewController.swift
+//  Pop
+//
+//  Created by Markus Jahnsrud on 28/10/2019.
+//  Copyright © 2019 Markus Jahnsrud. All rights reserved.
+//
+
+import UIKit
+
+class AlbumDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
